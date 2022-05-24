a53d91a0220696df85e1d63b69eade10146bca65
aarsh-zartek <aarsh@zartek.in>
2022-05-24 16:11:05 +0530

    addd check phone_number exists endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.users.views import DailyActivityView, UserViewSet, UserView
+from apps.users.views import CheckPhoneNumberExistsView, DailyActivityView, UserViewSet, UserView
 
 
 router = DefaultRouter()
@@ -11,5 +11,6 @@ router.register(prefix="user-update", viewset=UserViewSet, basename="user")
 urlpatterns = [
     path("", include(router.urls)),
     path("user/daily-activity", DailyActivityView.as_view(), name="user-daily-activity"),
+    path("user/check-phone-number", CheckPhoneNumberExistsView.as_view(), name="user-check-phone-number"),
     path("user", UserView.as_view(), name="user-details"),
 ]
