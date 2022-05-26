055f26bb6808fa7307a441fdb41104de2f1a12ab
aarsh-zartek <aarsh@zartek.in>
2022-05-26 15:01:10 +0530

    improved firebase template coloring

--- prosit/urls.py
+++ prosit/urls.py
@@ -26,9 +26,6 @@ from drf_yasg.views import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # path('api/', include("djoser.urls.jwt")),
-    # path('api/', include('rest_framework.urls')),
     
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
