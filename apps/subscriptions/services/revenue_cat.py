4bee90dff5ea58577b094ad4fe481b92a0e95c53
aarsh-zartek <aarsh@zartek.in>
2022-07-11 19:44:12 +0530

    added sample revenuecat response

--- apps/subscriptions/services/revenue_cat.py
+++ apps/subscriptions/services/revenue_cat.py
@@ -25,3 +25,50 @@ class RevenueCat:
 		
 		return response
 
+
+"""
+Sample Response from RevenueCat
+{
+    "request_date": "2022-07-11T06:34:15Z",
+    "request_date_ms": 1657521244881,
+    "subscriber": {
+        "entitlements": {
+            "test_plan": {
+                "expires_date": null,
+                "grace_period_expires_date": null,
+                "product_identifier": "test_plan",
+                "purchase_date": "2022-07-11T06:32:19Z"
+            }
+        },
+        "first_seen": "2022-07-11T06:04:04Z",
+        "last_seen": "2022-07-11T06:04:04Z",
+        "management_url": null,
+        "non_subscriptions": {
+            "test_plan": [
+                {
+                    "id": "98787dh5ss",
+                    "is_sandbox": true,
+                    "original_purchase_date": "2022-07-11T06:10:38Z",
+                    "purchase_date": "2022-07-11T06:10:38Z",
+                    "store": "play_store"
+                },
+                {
+                    "id": "98787dh5ss",
+                    "is_sandbox": true,
+                    "original_purchase_date": "2022-07-11T06:32:19Z",
+                    "purchase_date": "2022-07-11T06:32:19Z",
+                    "store": "play_store"
+                }
+            ]
+        },
+        "original_app_user_id": "$RCAnonymousID:253affa82fc84feca9dbd046836028cb",
+        "original_application_version": null,
+        "original_purchase_date": null,
+        "other_purchases": {
+            "test_plan": {"purchase_date": "2022-07-11T06:32:19Z"}
+        },
+        "subscriptions": {}
+    }
+}
+
+"""
