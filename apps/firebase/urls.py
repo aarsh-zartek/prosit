c8aa03caf3244905f29f3a271f2ccb121d8ca146
aarsh-zartek <aarsh@zartek.in>
2022-08-17 19:32:29 +0530

    updated firebase header, phone-[in]visible, views

--- apps/firebase/urls.py
+++ apps/firebase/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 # local
-from .views import Index, passthought
+from .views import Index, passthough
 
 app_name = "firebase"
 
 urlpatterns = [
-    path("<page>", passthought, name="passthought"),
+    path("<page>", passthough, name="passthough"),
     path("", Index.as_view(), name="index"),
 ]
